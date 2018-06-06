677ec61dfe4f55f531b58115b2bd622842326eb0
Josh Hollis <joshua.hollis@gmail.com>
2018-06-06 13:59:37 -0400

    prop value correction

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -10,7 +10,7 @@ class QuizQuestion extends Component {
                 </section>
                 <section className="buttons">
                 <ul>
-                    <QuizQuestionButton button_text="this.props.quiz_question.answer_options[0]"/>
+                    <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
                 </ul>
                 </section>
             </main>
